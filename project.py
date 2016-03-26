ab09636f0e15044c9d1580455e336182a13db9c3
David Menard <david.menard@autodesk.com>
2016-03-26 13:20:44 -0400

    tabs to space

--- project.py
+++ project.py
@@ -12,24 +12,24 @@ money_list = [] #List from which we send option symbols to a .csv file
 for i in tickers.index:
     option = Options(i,'yahoo')
     data = option.get_all_data()
-	
-	# The returned data is a pandas DataFrame: http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html
-	# RTFM
+    
+    # The returned data is a pandas DataFrame: http://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.html
+    # RTFM
     
     if not data.empty :
-		i = 0
-		while i < 5:
+        i = 0
+        while i < 5:
 
-			try:
-				
-				#money_list.append(data[data.Vol > 1000].index.get_level_values('Symbol'))
-				print data[Symbol] 
+            try:
+                
+                #money_list.append(data[data.Vol > 1000].index.get_level_values('Symbol'))
+                print data[Symbol] 
 
-			except Exception as e:
-				print "ERROR: " + str(e)
-			
-			time.sleep(1)
-			i += 1
+            except Exception as e:
+                print "ERROR: " + str(e)
+            
+            time.sleep(1)
+            i += 1
 
 new_file = open("output.csv", 'wb') #Fix: 1)make it write to .csv in rows 2) skipping lines?
 writer = csv.writer(new_file)       #Should I have them written in the function above?
